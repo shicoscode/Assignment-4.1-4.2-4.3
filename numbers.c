552fd28a573acd59519e3bb97a216eaf97139da1
shicoscode <164689852+shicoscode@users.noreply.github.com>
2024-06-28 10:17:34 +0300

    The smallest number.c
    Prompt the user for 10 numbers, Output the smallest. (4.1)

---
+++ numbers.c
@@ -0,0 +1,27 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// 4.1 Write a C program that reads a set of 10 numbers and finds and prints the smallest number.
+
+int main(void)
+{
+    int n = 10;
+    int numbers[n];
+
+    // Initialization
+    for (int i = 0; i < n; i++)
+    {
+        numbers[i] = get_int("Number: ");
+    }
+    int smallest = numbers[0];
+    
+    for (int i = 1; i < n ; i++)
+    {
+        if (numbers[i] < smallest)
+        {
+            smallest number = numbers[i];
+        }
+    }
+    printf("smallest=%i;", smallest);
+}
